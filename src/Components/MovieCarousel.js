b727173046976bf5c9d1eda6334b65f8db615385
Yash Dand <yash.hastechy@gmail.com>
2022-08-30 16:06:00 +0530

    Changes

--- src/Components/MovieCarousel.js
+++ src/Components/MovieCarousel.js
@@ -20,6 +20,7 @@ const MovieCarousel = ({ fetchAPI }) => {
                 );
         }
         fetchData();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [fetchAPI]);
 
     var settings = {
